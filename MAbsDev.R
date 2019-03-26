3b039d18e92968137a989b394e82368bd18fec0f
Lucas Braga <lucasbraga461@users.noreply.github.com>
2019-03-26 12:00:47 -0300

    Update MAbsDev.R

--- MAbsDev.R
+++ MAbsDev.R
@@ -1,12 +1,19 @@
-# the input dasetCol is the column of a data frame, example: Tukey(iris$Sepal.Length)
+# the input dasetCol is the column of a data frame, example: MAbsDev(iris$Sepal.Length)
 
-# Median Absolute Deviation method for Normal or approx. Normal Distributions:
+# Median Absolute Deviation method:
 #   $\sigma_M = M(|x_i - M(x)|)$ #LaTeX
 #   $M(x) - 3\sigma_M * 1.4826 < x_i < M(x) + 3\sigma_M * 1.4826$ #LaTeX
 MAbsDev <- function(dasetCol) {
+  dasetCol <- dasetCol[!is.na(dasetCol)]
   sigma_M <- median(abs(dasetCol-median(dasetCol)))
   low_M <- median(dasetCol) - 3*sigma_M*1.4826
   high_M <- median(dasetCol) + 3*sigma_M*1.4826
   Limits <- c(low_M, high_M)
-  return(Limits)
+  if (length(dasetCol[dasetCol<Limits[1]])==0 & length(dasetCol[dasetCol>Limits[2]])==0) {
+    return(paste("lower limit:", Limits[1], " upper limit:", Limits[2], "outliers: none"))
+  }
+  return(paste("lower limit:", Limits[1], " upper limit:", Limits[2],
+               "outliers:",
+               list(unique(sort(dasetCol[dasetCol<Limits[1]])),
+               unique(sort(dasetCol[dasetCol>Limits[2]])))))
 }
